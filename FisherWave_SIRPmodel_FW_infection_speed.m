12f9a13b5506a9a43774d746af62182fc9031930
YiZhangsiat <107541089+YiZhangsiat@users.noreply.github.com>
2024-04-26 15:29:48 +0800

    Analysis code for Fig.S5

--- FisherWave_SIRPmodel_FW_infection_speed.m
+++ FisherWave_SIRPmodel_FW_infection_speed.m
@@ -1,8 +1,3 @@
-%%%%%%%% ”–«˜ªØ◊˜”√µƒ«Èøˆœ¬£¨‘⁄«˜ªØœµ ˝∫‹–°£¨∏√¿©’≈ƒ£ Ω∫Õfisherwave¿‡À∆ ±£¨º∆À„«÷»æ√Êª˝º∞∏˜ÀŸ∂»°£
-%%%%%%%%  ◊œ»º∆À„◊›œÚ…œTotal_cell=0.9µƒŒª÷√FW_lŒ™frontwave…œÃÿ∂®µƒŒª÷√µ„£ª
-%%%%%%%% ‘Ÿ‘⁄∏√Œª÷√¥¶∫·œÚ∑ΩœÚ≤È’“infected cell µƒº´÷µµ„Œ™«÷»æ«¯”Úµƒ◊Û”“±ﬂΩÁ£ª
-%%%%%%%% ƒ‚∫œ◊Û”“±ﬂΩÁ£¨¥”¡Ω±ﬂΩÁµƒΩπµ„ø™ º÷¡frontwave…œFW_l∑∂Œßƒ⁄º∆À„»˝Ω«–Œ√Êª˝£¨º¥Œ™«÷»æ√Êª˝£ª
-
 clear all;
 close all;
 
@@ -17,7 +12,7 @@ for m=1:length(TimeT)
     filename1=strcat('initphage=1_GR=30_kap=2.5_PR=15_P0=0.05_Nk=5_Diff=',int2str(Diff_value(i)),'_',int2str(TimeT(m)));
 
      load(strcat(filename1,'.mat'));
-     %%  frontwave…œinfected cell∞Î≤®≥§µƒº∆À„
+     %%  
      Total_cell=Cell_den_S+Cell_den_I+Cell_den_R;
      infected_cell=Cell_den_I+Cell_den_R;
      
@@ -25,10 +20,10 @@ for m=1:length(TimeT)
 
   
      
-     [~,FW_loca]=min(abs(Total_cell(ref_value:end,50)-1));  %º∆À„fisherwave…œtotal densityŒ™0.9µƒŒª÷√
+     [~,FW_loca]=min(abs(Total_cell(ref_value:end,50)-1));  %ËÆ°ÁÆófisherwave‰∏ätotal density‰∏∫0.9ÁöÑ‰ΩçÁΩÆ
       FW_l=FW_loca(end)+ref_value-1;
           
-     %º∆À„πÃ∂®Œª÷√¥¶«÷»æ√Êª˝µƒ◊Û∞Î≤®∫Õ”“∞Î≤®
+     %
      infect_ref_value=0.0001;
 %      LY_C=round(round(LY/dx)/2);
 %          [~,FW_Cell_Infect_half_loc_B]=min(abs(infected_cell(FW_l,1:LY_C)- infect_ref_value));
@@ -38,12 +33,8 @@ for m=1:length(TimeT)
 %          FW_location_y_left(m)=FW_Cell_Infect_half_loc_B;
 %          FW_location_y_right(m)=FW_Cell_Infect_half_loc_E+LY_C-1;
 % 
-%          FW_Infection_range(m)=(FW_location_y_right(m)-FW_location_y_left(m))*dx/10000; 
-%          
-         
-         
-         
-         
+%          FW_Infection_range(m)=(FW_location_y_right(m)-FW_location_y_left(m))*dx/10000;      
+  
      
      
      [FWpeak_v,FWpeak_l]=findpeaks(infected_cell(FW_l,:));
@@ -62,17 +53,17 @@ for m=1:length(TimeT)
       
       end
 
-     %%  Exp_MR µƒº∆À„  peak information of infected Cell
+     %%  Exp_MR 
          fixed_exp_V=round(LY/dy)/2-50;
      
-         [~,FW_exp_loca]=min(abs(Total_cell(ref_value:end,fixed_exp_V)-0.5));  %º∆À„fisherwave…œtotal densityŒ™0.9µƒŒª÷√
+         [~,FW_exp_loca]=min(abs(Total_cell(ref_value:end,fixed_exp_V)-0.5));  %ËÆ°ÁÆófisherwave‰∏ätotal density‰∏∫0.9ÁöÑ‰ΩçÁΩÆ
          FW_exp_l=FW_exp_loca(end)+ref_value-1;
 
          cell_infected_peak_location(m)=FW_exp_l*dx/10000; %cm
          cell_infected_peak_value(m)=Total_cell(FW_exp_l,fixed_exp_V);
      
      
-   %% Con_MRµƒº∆À„  peak information of  Mix Cell
+   %% Con_MR
      FW_con_l=FW_l;
      cell_con_peak_location(m)=FW_con_l*dx/10000; %cm
      cell_con_peak_value(m)=Total_cell(FW_con_l,100);
@@ -80,7 +71,7 @@ for m=1:length(TimeT)
 end
 end
 
-  %% migration velocity Cell  µ—È◊È
+  %% migration velocity Cell 
   real_time_h=TimeT;
 cell_infected_peak_time=real_time_h;
 cell_infected_peak_time(isnan(cell_infected_peak_location))=[];
@@ -89,7 +80,7 @@ MR_cell_infected_para=polyfit(cell_infected_peak_time,cell_infected_peak_locatio
 MR_cell_infected=MR_cell_infected_para(1)*cell_infected_peak_time+MR_cell_infected_para(2);
 cell_infected_MR_V=MR_cell_infected_para(1);
 % R2_Cell_infected_line(i,:)=norm(MR_cell_infected-mean(cell_infected_peak_location))^2/norm(cell_infected_peak_location-mean(cell_infected_peak_location))^2;
-%% migration velocity Cell ∂‘’’◊È
+%% migration velocity Cell 
 cell_con_peak_time=real_time_h;
 cell_con_peak_time(isnan(cell_con_peak_location))=[];
 cell_con_peak_location(isnan(cell_con_peak_location))=[];
@@ -98,83 +89,15 @@ MR_cell_con=MR_cell_con_para(1)*cell_con_peak_time+MR_cell_con_para(2);
 cell_con_MR_V=MR_cell_con_para(1);
 % R2_Cell_con_line(i,:)=norm(MR_cell_con-mean(cell_con_peak_location))^2/norm(cell_con_peak_location-mean(cell_con_peak_location))^2;
 
-%% infection speed  V//µƒº∆À„
+%% infection speed  V//
 cell_infection_time=real_time_h;
 Infection_speed_para=polyfit(cell_infection_time,FW_Infection_range,1);
 Infection_speed_line=Infection_speed_para(1)*cell_infection_time+Infection_speed_para(2);
 Infection_speed_V=Infection_speed_para(1);
 
-%% «÷»æ±ﬂΩÁƒ‚∫œ ◊Û±ﬂΩÁ ∫·◊¯±Í «FW_location_y_left£¨◊›◊¯±Í «FW_location_x£®infected_area_left_line£©
-infected_area_left_para=polyfit(FW_location_y_left,FW_location_x,1);
-infected_area_left_line=infected_area_left_para(1)*FW_location_y_left+infected_area_left_para(2);
-%% «÷»æ±ﬂΩÁƒ‚∫œ ”“±ﬂΩÁ ∫·◊¯±Í «FW_location_y_right£¨◊›◊¯±Í «FW_location_x£®infected_area_right_line£©
-infected_area_right_para=polyfit(FW_location_y_right,FW_location_x,1);
-infected_area_right_line=infected_area_right_para(1)*FW_location_y_right+infected_area_right_para(2);
-%% «÷»æ√Êª˝µƒº∆À„  “—÷™◊›◊¯±Íinfection_X_range  «ÛΩ‚∫·◊¯±Íleft_location£¨ ƒ‚∫œ«˙œﬂ”¶∑¥π˝¿¥£ª∫·◊¯±Íµƒ≤Ó÷µ «√ø“ª––V-shapeµƒ√Êª˝≤Ó÷µ
-% Location_x=(infected_area_right_para(2)-infected_area_left_para(2))/(infected_area_left_para(1)-infected_area_right_para(1));
-%  focal_point_temp=infected_area_left_para(1)*Location_x+infected_area_left_para(2);
-%  focal_point=round(focal_point_temp);
 
-infection_X_range=1:FW_l;
 
-% infection_X_range=round(LXg0/dx):FW_l(length(FW_l));
-for k=1:length(infection_X_range)
-    left_location(k)=(infection_X_range(k)-infected_area_left_para(2))/infected_area_left_para(1);
-    right_location(k)=(infection_X_range(k)-infected_area_right_para(2))/infected_area_right_para(1);
-    infected_distance_X(k)=right_location(k)-left_location(k);
-end  
-
-[line_focus_v, line_focus_loc]=min(abs(round(left_location)-round(right_location)));
-
- if  ~isnan(line_focus_loc)
-     infected_area=sum(infected_distance_X(line_focus_loc:end))*dx*dy;
-     area_R=(length(infection_X_range)-line_focus_loc)*dx; 
-     infected_ratio=2*infected_area/area_R/area_R;
- else
- infected_area=sum(infected_distance_X)*dx*dy;
- area_R=length(infection_X_range)*dx; 
- infected_ratio=infected_area/area_R/area_R;
- end 
- 
- %% V-shape Ω«∂»µƒº∆À„
- L_vect=[left_location(end)-left_location(1),infection_X_range(end)-infection_X_range(1)];
- R_vect=[right_location(end)-right_location(1),infection_X_range(end)-infection_X_range(1)];
- V_vect_angle=acos(dot(L_vect,R_vect)/(norm(L_vect)*norm( R_vect)));
- %% Õº∆¨ V-shape ±ﬂΩÁº∞∆‰ƒ‚∫œœﬂ
-Cell_line=figure('position',[100 100 500 500]);
-imagesc(infected_cell);hold on;
-plot(FW_location_y_left,FW_location_x,'linewidth',4);hold on;
-plot(FW_location_y_right,FW_location_x,'linewidth',4);
-saveas(Cell_line,strcat(filename,'_Cell_line'),'fig');
-saveas(Cell_line,strcat(filename,'_Cell_line'),'png');
-
- %% Õº∆¨ V-shape ±ﬂΩÁº∞∆‰ƒ‚∫œœﬂ
- Cell_simu_fit_line=figure('position',[100 100 500 500]);
-imagesc(Total_cell);hold on;
- plot(FW_location_y_left,FW_location_x,'linewidth',4);hold on;
- plot(left_location,infection_X_range,'linewidth',4); hold on;
- plot(FW_location_y_right,FW_location_x,'linewidth',4);hold on;
- plot(right_location,infection_X_range,'linewidth',4); hold on;
- text(round(LY/dy)/2,round(LX/dx)/2,sprintf('infection-area-ratio=%.3f',infected_ratio),'FontSize',30);
- text(round(LY/dy)/3,round(LX/dx)/3,sprintf('IS-to-MR=%.3f',Infection_speed_V/cell_con_MR_V/2),'FontSize',30);
- text(round(LY/dy)/5,round(LX/dx)/5,sprintf('V-vect-angle=%.3f',V_vect_angle),'FontSize',30);
- saveas(Cell_simu_fit_line,strcat(filename,'_Cell_simu_fit_line'),'fig');
-saveas(Cell_simu_fit_line,strcat(filename,'_Cell_simu_fit_line'),'png');
-
-%% infection range º∞∆‰ƒ‚∫œœﬂ
-FW_IR=figure('position',[100 100 500 500]);
-plot(real_time_h,FW_Infection_range,'linewidth',4); hold on;
-plot(cell_infection_time,Infection_speed_line,'r');hold on;
-text(cell_infection_time(end-10),FW_Infection_range(end-10),sprintf('IS=%.3f',Infection_speed_V),'FontSize',30);
-
-xlabel('time');
-ylabel('infection range');
-% legend('chi=200');
-title('front wave infection range');
-set(gca,'linewidth',3,'FontSize',30,'LineWidth',3);
-saveas(FW_IR,strcat(filename,'_Infection_speed'),'fig');
-saveas(FW_IR,strcat(filename,'_Infection_speed'),'png');
-%%   µ—È◊È ÀŸ∂»º∞∆‰ƒ‚∫œ
+%%  
 Cell_Exp_peak_infor=figure('position',[100 100 500 500]);
 subplot(2,1,1);plot(real_time_h,cell_infected_peak_value); 
 text(real_time_h(end-10),cell_infected_peak_value(end-10),sprintf('peak value=%.3f',max(cell_infected_peak_value)),'FontSize',30);
@@ -192,7 +115,7 @@ ylabel('Cell infected peak location');
 saveas(Cell_Exp_peak_infor,strcat(filename,'_Exp_MR'),'fig');
 saveas(Cell_Exp_peak_infor,strcat(filename,'_Exp_MR'),'png');
 
-%% ∂‘’’◊ÈÀŸ∂»º∞∆‰ƒ‚∫œ
+%% 
 Cell_con_peak_infor=figure('position',[100 100 500 500]);
 subplot(2,1,1);plot(real_time_h,cell_con_peak_value); 
 text(real_time_h(end-10),cell_con_peak_value(end-10),sprintf('peak value=%.3f',max(cell_con_peak_value)),'FontSize',30);
@@ -209,57 +132,4 @@ ylabel('Cell con peak location');
 saveas(Cell_con_peak_infor,strcat(filename,'_con_MR'),'fig');
 saveas(Cell_con_peak_infor,strcat(filename,'_con_MR'),'png');
 
-% %%
-% C_IR=figure('position',[100 100 1500 900]);
-% plot(TimeT,C_Infection_range,'linewidth',4); 
-% xlabel('time');
-% ylabel('infection range');
-% title('Cell_den(400,:) infection range');
-% legend('chi=60','chi=90','chi=120','chi=150','chi=180')
-% set(gca,'linewidth',3,'FontSize',30,'LineWidth',3);
-% saveas(C_IR,strcat(filename,'_C_IR'),'fig');
-% saveas(C_IR,strcat(filename,'_C_IR'),'png');
 
-%%
-% FW_Cell_1_IR=figure('position',[100 100 1500 900]);
-% 
-% % h1=plot(Cell_den(FW_l(end),:,1)+Cell_den(FW_l(end),:,2),'linewidth',4,'Color','black');hold on;
-% % h2=plot(Cell_den(FW_l(end),:,1),'linewidth',3,'Color',[0 0.5 0]);hold on;
-% h3=plot(Total_cell(FW_l(end),:),'linewidth',3,'Color','red');hold on;
-% plot(FW_Cell_1_half_loc_B,Total_cell(FW_l(end),FW_Cell_1_half_loc_B),'*','Color','black');hold on;
-%  plot(FW_Cell_1_half_loc_E,Total_cell(FW_l(end),FW_Cell_1_half_loc_E),'*','Color','black');hold on;
-% % xlim([0 320]);
-% % ylim([0 1.3]);
-% xlabel('Distance');
-% % ylabel('OD_6_0_0');
-% % set(gca,'xtick',[0 100 200 300]);
-% % set(gca,'ytick',[0 0.4 0.8 1.2]);
-% set(gca,'linewidth',3,'FontSize',30,'LineWidth',3);
-% % l1=legend([h1 h2 h3],{'total cell','uninfected cell','infected cell'},'location','Northwest');
-% % set(l1,'Fontname', 'Times New Roman','FontWeight','bold','FontSize',30,'box','off');
-% % text(250,max(Nutr(400,300))+0.04,'rel.nutrient','FontSize',30);
-% title('Front wave infection range')
-% saveas(FW_Cell_1_IR,strcat(filename,'_cell'),'fig');
-% saveas(FW_Cell_1_IR,strcat(filename,'_cell'),'png');
-
-%  %%
-% h=figure('position',[100 100 1500 900]);
-% c=area(Nutr(400,:),'LineStyle',':');c.FaceColor = [1 0.7 0.2];hold on;
-% h1=plot(Cell_den(400,:,1)+Cell_den(400,:,2),'linewidth',4,'Color','black');hold on;
-% h2=plot(Cell_den(400,:,1),'linewidth',3,'Color',[0 0.5 0]);hold on;
-% h3=plot(Cell_den(400,:,2),'linewidth',3,'Color','red');hold on;
-% plot(Cell_2_half_loc_B,Cell_den(400,Cell_2_half_loc_B,2),'*','Color','black');hold on;
-% plot(Cell_2_half_loc_E,Cell_den(400,Cell_2_half_loc_E,2),'*','Color','black');hold on;
-% xlim([0 320]);
-% ylim([0 1.3]);
-% xlabel('Distance');
-% % ylabel('OD_6_0_0');
-% set(gca,'xtick',[0 100 200 300]);
-% set(gca,'ytick',[0 0.4 0.8 1.2]);
-% set(gca,'linewidth',3,'FontSize',30,'LineWidth',3);
-% l1=legend([h1 h2 h3],{'total cell','uninfected cell','infected cell'},'location','Northwest');
-% set(l1,'Fontname', 'Times New Roman','FontWeight','bold','FontSize',30,'box','off');
-% text(250,max(Nutr(400,300))+0.04,'rel.nutrient','FontSize',30);
-% title('Cell_den(400,:) infection range')
-% saveas(h,strcat(filename,'_cell'),'fig');
-% saveas(h,strcat(filename,'_cell'),'png');
