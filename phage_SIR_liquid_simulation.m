21350325d0a0a760327dae7b49f6af8918ca73a8
YiZhangsiat <107541089+YiZhangsiat@users.noreply.github.com>
2024-11-15 16:56:31 +0800

    simulation code of SIR model for liquid culture

---
+++ phage_SIR_liquid_simulation.m
@@ -0,0 +1,14 @@
+clear all;
+close all;
+
+phage_SIR_liquid_function('initial_phage=0.mat',0);
+phage_SIR_liquid_function('initial_phage=1E-4.mat' ,1E-4);
+phage_SIR_liquid_function('initial_phage=1E-2.mat',1E-2);
+phage_SIR_liquid_function('initial_phage=1.mat',1);
+
+  
+  
+  
+
+
+
